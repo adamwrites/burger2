3c8d0686e89640315c79a91a086ab1a43f121cc6
Adam Tatar <adam@wikileaf.com>
2020-11-05 10:50:23 -0500

    base application

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Cheeseburger', FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Chicken Burger', FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Mushroom Burger', FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Bacon Burger', FALSE);
